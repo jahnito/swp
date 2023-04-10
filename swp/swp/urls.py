ef8b2f9e386f5fdf20735c07852b2ea67ecbfabe
jahnito <jahnito@gmail.com>
2023-04-10 18:00:17 +0500

    Created first app Meetings

---
+++ swp/swp/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for swp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django_jinja import views
+
+
+handler400 = views.BadRequest.as_view()
+handler403 = views.PermissionDenied.as_view()
+handler404 = views.PageNotFound.as_view()
+handler500 = views.ServerError.as_view()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
